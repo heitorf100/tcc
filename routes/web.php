b350d4aebd08fbfc97e0c7ae60c695f7c49dd3e7
Heitor Felipe <heitorf100@gmail.com>
2025-09-18 22:39:55 -0300

    3

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', [UserController::class,'index'])->name('user.index');
+Route::get('/photos/create',[PhotoController::class,'create'])->name('photos.create');
+Route::post('/photos',[PhotoController::class,'store'])->name('photos.store');
+Route::get('/photos/{photo}',[PhotoController::class,'show'])->name('photo.show');
+Route::get('/photos/{photo}/edit',[PhotoController::class,'edit'])->name('photo.edit');
+Route::put('/photos/{photo}',[PhotoController::class,'update'])->name('photo.update');
+Route::delete('/photos/{photo}',[PhotoController::class,'destroy'])->name('photo.destroy');
